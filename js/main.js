fffd6b82c2285f11fa648f1c811d2b954262b35a
Frances Hughes <franceslhughes@hotmail.com>
2022-12-15 12:42:56 +1300

    completed

--- js/main.js
+++ js/main.js
@@ -39,3 +39,53 @@ addInput.addEventListener('keyup', (event) => {
     addLists();
   }
 });
+
+const listUl = document.querySelector('.list');
+const lis = listUl.children;
+
+function createButton(li) {
+  // remove button
+  const remove = document.createElement('button');
+  remove.className = 'btn-icon remove';
+  li.appendChild(remove);
+
+  // down button
+  const down = document.createElement('button');
+  down.className = 'btn-icon down';
+  li.appendChild(down);
+
+  // up button
+  const up = document.createElement('button');
+  up.className = 'btn-icon up';
+  li.appendChild(up);
+
+  return li;
+}
+
+// loop to add buttons in each li
+for (var i = 0; i < lis.length; i++) {
+  createButton(lis[i]);
+}
+
+// buttons can move item up, down or delete item.
+
+divList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const button = event.target;
+    const li = button.parentNode;
+    const ul = li.parentNode;
+    if (button.className === 'btn-icon remove') {
+      ul.removeChild(li);
+    } else if (button.className === 'btn-icon down') {
+      const nextLi = li.nextElementSibling;
+      if (nextLi) {
+        ul.insertBefore(nextLi, li);
+      }
+    } else if (button.className === 'btn-icon up') {
+      const prevLi = li.previousElementSibling;
+      if (prevLi) {
+        ul.insertBefore(li, prevLi);
+      }
+    }
+  }
+});
